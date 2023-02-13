9f5b0993f30bc58e54d0960213e0858d28842590
xEphyrrrr <bernardbereness78@gmail.com>
2023-02-13 23:30:18 +0700

    first commit

---
+++ laravel/resources/views/aboutus.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/styleabout.css') }}">
+    <title>About Us</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="Else"><a href="/">Home</a></div>
+        <div class="Else"><a href="/Listworker">List Worker</a></div>
+        <div class="About"><a href="/aboutus">About Us</a></div>
+    </div>
+
+    <div class="page">
+        <div><h1>About Us</h1></div>
+        <div>
+            PT Mexico adalah sebuah perusahaan yang bergerak dalam bidang software house yang telah mengerjakan proyek client sebanyak 600.000 proyek per tahunnya.
+        </div>
+    </div>
+</body>
+</html>
